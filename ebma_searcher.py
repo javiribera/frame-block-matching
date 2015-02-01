af7c92dea49043a607ede5a633a1830b8f1dde2b
Javi Ribera <javiribera@gmail.com>
2015-02-01 17:53:14 -0500

    read binary images and store to PNG files

--- ebma_searcher.py
+++ ebma_searcher.py
@@ -1,20 +1,24 @@
-__author__ = 'javiribera'
 """
 Estimates the motion between to frame images
  by running an Exhaustive search Block Matching Algorithm (EBMA).
 
+Tried in Python 2.7.5
+
 !! DEPENDENCIES !!
 This script depends on the following Python Packages:
 - argparse
+- scikit-image
 - numpy
 - matplotlib
 """
 
 import argparse
-from utils import positive_integer
-import numpy as np
-import matplotlib
-from ipdb import set_trace as debug
+import os
+
+from skimage.io import imsave
+
+from utils import positive_integer, read_binary_image
+
 
 if __name__ == "__main__":
 
@@ -22,12 +26,30 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='EBMA searcher. '
                                                  'Estimates the motion between to frame images '
                                                  ' by running an Exhaustive search Block Matching Algorithm (EBMA).')
-    parser.add_argument('--video', dest='video_path', required=True, type=str,
-                        help='Path of the video to analyze.')
+    parser.add_argument('--target-frame', dest='target_frame_path', required=True, type=str,
+                        help='Path to the frame that will be predicted.')
+    parser.add_argument('--anchor-frame', dest='anchor_frame_path', required=True, type=str,
+                        help='Path to the frame that will be used to predict the target frame.')
+    parser.add_argument('--frame-width', dest='frame_width', required=True, type=positive_integer,
+                        help='Frame width.')
+    parser.add_argument('--frame-height', dest='frame_height', required=True, type=positive_integer,
+                        help='Frame height.')
+    parser.add_argument('--exponent', dest='p', required=False, type=float, default=1,
+                        help='Exponent used for the DFD. p=1 => MAD, p=2 => MSE. Default: p=1.')
     parser.add_argument('--block-size', dest='block_size', required=True, type=positive_integer,
                         help='Size of the blocks the image will be cut into, in pixels.')
     parser.add_argument('--search-range', dest='search_range', required=True, type=positive_integer,
                         help="Range around the pixel where to search, in pixels.")
     args = parser.parse_args()
 
-    debug()
+    width = args.frame_width
+    height = args.frame_height
+
+    # Pixel map of the frames in the [0,255] interval
+    target_frame = read_binary_image(args.target_frame_path, (width, height))
+    anchor_frame = read_binary_image(args.anchor_frame_path, (width, height))
+
+    # store frames in PNG
+    os.system('mkdir -p frames_being_processed')
+    imsave('frames_being_processed/target.png', target_frame)
+    imsave('frames_being_processed/anchor.png', anchor_frame)
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ __author__ = 'javiribera'
 
 import argparse
 
+import numpy as np
+
+
 def positive_integer(number):
     """
     Convert a number to an positive integer if possible.
@@ -17,3 +20,20 @@ def positive_integer(number):
             raise argparse.ArgumentTypeError('%s is not a positive integer' % number)
     except ValueError:
         raise argparse.ArgumentTypeError('%s is not a positive integer' % number)
+
+
+def read_binary_image(path, (height, width)):
+    """
+    Read binary gray-scale image. Each pixel is considered stored in contiguous bytes.
+    :param path: Path to the image to read.
+    :param (height, width): Height and width of the image.
+    :return: The numpy array containing the resulting image.
+    """
+
+    # open file in binary format
+    with open(path, "rb") as fd:
+        whole_file = fd.read(height * width)
+    pixels = np.array([ord(byte) for byte in whole_file], dtype=np.uint8)  # get ASCII value of each byte of the file
+    pixels = pixels.reshape((width, height))
+
+    return pixels
