ccdac49624707f1ca65fe6c8f974b616ddd7066c
Javi Ribera <javiribera@gmail.com>
2015-02-02 22:33:27 -0500

    +ebma method to predict the target frame and show motion field

--- ebma_searcher.py
+++ ebma_searcher.py
@@ -1,6 +1,7 @@
 """
 Estimates the motion between to frame images
  by running an Exhaustive search Block Matching Algorithm (EBMA).
+Minimizes the norm of the Displaced Frame Difference (DFD).
 
 Tried in Python 2.7.5
 
@@ -14,14 +15,20 @@ This script depends on the following Python Packages:
 
 import argparse
 import os
+import itertools
+
+import numpy as np
+
+
+
+# import timeit
 
 from skimage.io import imsave
 
-from utils import positive_integer, read_binary_image
+from utils import positive_integer, read_binary_image, subarray, show_quiver
 
 
-if __name__ == "__main__":
-
+def main():
     # parse the command line arguments to attributes of 'args'
     parser = argparse.ArgumentParser(description='EBMA searcher. '
                                                  'Estimates the motion between to frame images '
@@ -34,22 +41,99 @@ if __name__ == "__main__":
                         help='Frame width.')
     parser.add_argument('--frame-height', dest='frame_height', required=True, type=positive_integer,
                         help='Frame height.')
-    parser.add_argument('--exponent', dest='p', required=False, type=float, default=1,
-                        help='Exponent used for the DFD. p=1 => MAD, p=2 => MSE. Default: p=1.')
+    parser.add_argument('--norm', dest='norm', required=False, type=float, default=1,
+                        help='Norm used for the DFD. p=1 => MAD, p=2 => MSE. Default: p=1.')
     parser.add_argument('--block-size', dest='block_size', required=True, type=positive_integer,
                         help='Size of the blocks the image will be cut into, in pixels.')
     parser.add_argument('--search-range', dest='search_range', required=True, type=positive_integer,
                         help="Range around the pixel where to search, in pixels.")
     args = parser.parse_args()
 
+    # size of all the frames
     width = args.frame_width
     height = args.frame_height
 
     # Pixel map of the frames in the [0,255] interval
-    target_frame = read_binary_image(args.target_frame_path, (width, height))
-    anchor_frame = read_binary_image(args.anchor_frame_path, (width, height))
+    target_frame = read_binary_image(args.target_frame_path, (height, width))
+    anchor_frame = read_binary_image(args.anchor_frame_path, (height, width))
 
-    # store frames in PNG
+    # store frames in PNG for our records
     os.system('mkdir -p frames_being_processed')
     imsave('frames_being_processed/target.png', target_frame)
     imsave('frames_being_processed/anchor.png', anchor_frame)
+
+    # block size
+    N = args.block_size
+
+    # search range
+    R = args.search_range
+
+    # the p-norm of the DFD will be minimized
+    p = args.norm
+
+    # predicted frame. target_frame is predicted from anchor_frame
+    predicted_frame = np.empty(((height, width)), dtype=np.uint8)
+
+    # optical flow consisting in the displacement of each block in vertical and horizontal
+    optical_flow_x = np.empty((int(height / N), int(width / N)))
+    optical_flow_y = np.empty((int(height / N), int(width / N)))
+
+    # loop through every NxN block in the image
+    for (blk_row, blk_col) in itertools.product(xrange(0, height - (N - 1), N),
+                                                xrange(0, width - (N - 1), N)):
+
+        # block whose match will be searched in the anchor frame
+        blk = target_frame[blk_row:blk_row + N, blk_col:blk_col + N]
+
+        # minimum norm of the DFD norm found so far
+        dfd_n_min = np.infty
+
+        # search which block in a surrounding RxR region minimizes the norm of the DFD. Blocks overlap.
+        for (r_col, r_row) in itertools.product(range(-R, R),
+                                                range(-R, R)):
+            # candidate block upper left vertex and lower right vertex position as (row, col)
+            up_l_candidate_blk = (blk_row + r_row, blk_col + r_col)
+            low_r_candidate_blk = (blk_row + r_row + N - 1, blk_col + r_col + N - 1)
+
+            # # don't search outside the anchor frame
+            # if up_l_candidate_blk[0] < 0 or up_l_candidate_blk[1] < 0 \
+            # or low_r_candidate_blk[0] > height - 1 or low_r_candidate_blk[1] > width -1:
+            # continue
+
+            # the candidate block may fall outside the anchor frame
+            candidate_blk = subarray(anchor_frame, up_l_candidate_blk, low_r_candidate_blk)
+            assert candidate_blk.shape == (N, N)
+
+            dfd = np.array(candidate_blk, dtype=np.float64) - np.array(blk, dtype=np.float64)
+
+            candidate_dfd_norm = np.linalg.norm(dfd, ord=p)
+
+            # a better matching block has been found. Save it and its displacement
+            if candidate_dfd_norm < dfd_n_min:
+                dfd_n_min = candidate_dfd_norm
+                matching_blk = candidate_blk
+                dy = r_col
+                dx = r_row
+
+        # construct the predicted image with the block that matches this block
+        predicted_frame[blk_row:blk_row + N, blk_col:blk_col + N] = matching_blk
+
+        # displacement of this block in each direction
+        print str((blk_row / N, blk_col / N)) + '---' + str((dx, dy))
+
+        optical_flow_x[blk_row / N, blk_col / N] = dx
+        optical_flow_y[blk_row / N, blk_col / N] = dy
+
+    # store predicted frame
+    imsave('frames_being_processed/predicted_target.png', predicted_frame)
+
+    # show optical flow
+    show_quiver(optical_flow_x[::-1], optical_flow_y[::-1])
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+    # tictoc = timeit.timeit(main, number=1)
+    # print 'Time to run the whole main(): %s' %tictoc
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
-__author__ = 'javiribera'
-
 import argparse
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -34,6 +33,101 @@ def read_binary_image(path, (height, width)):
     with open(path, "rb") as fd:
         whole_file = fd.read(height * width)
     pixels = np.array([ord(byte) for byte in whole_file], dtype=np.uint8)  # get ASCII value of each byte of the file
-    pixels = pixels.reshape((width, height))
+    pixels = pixels.reshape((height, width))
 
     return pixels
+
+
+def show_gray_img(asd):
+    plt.imshow(asd, cmap='gray')
+    plt.show()
+
+
+def show_quiver(x_component_arrows, y_components_arrows):
+    plt.quiver(x_component_arrows, y_components_arrows)
+    plt.show()
+
+
+def subarray(array, (upper_left_pix_row, upper_left_pix_col), (lower_right_pix_row, lower_right_pix_col)):
+    """
+    Return a subarray containing the pixels delimited by the pixels between upper_left_pix and lower_right.
+    If asked for pixels outside the image boundary, such pixels have value 0.
+    """
+
+    if upper_left_pix_row > lower_right_pix_row or upper_left_pix_col > lower_right_pix_col:
+        raise ValueError('coordinates of the subarray should correspond to a meaningful rectangle')
+
+    orig_array = np.array(array)
+
+    num_rows = lower_right_pix_row - upper_left_pix_row + 1
+    num_cols = lower_right_pix_col - upper_left_pix_col + 1
+
+    subarr = np.zeros((num_rows, num_cols), dtype=orig_array.dtype)
+
+    # zoomed outside the original image
+    if lower_right_pix_col < 0 or lower_right_pix_row < 0 or \
+                    upper_left_pix_col > orig_array.shape[1] - 1 or upper_left_pix_row > orig_array.shape[0] - 1:
+        return subarr
+
+    # region of the original image that is inside the desired region
+    # (i = col, j=row)
+    # _________________________________
+    # |                                | original image
+    # |   _____________________________|____
+    # |   |(j_o_1, i_o_1)              |    |
+    # |   |             (j_o_2, i_o_2) |    |
+    # |___|____________________________|    |
+    # |                                 |
+    # |_________________________________|  sliced final image
+
+    if upper_left_pix_col < 0:
+        i_o_1 = 0
+    else:
+        i_o_1 = upper_left_pix_col
+    if upper_left_pix_row < 0:
+        j_o_1 = 0
+    else:
+        j_o_1 = upper_left_pix_row
+
+    if lower_right_pix_col > orig_array.shape[1] - 1:
+        i_o_2 = orig_array.shape[1] - 1
+    else:
+        i_o_2 = lower_right_pix_col
+    if lower_right_pix_row > orig_array.shape[0] - 1:
+        j_o_2 = orig_array.shape[0] - 1
+    else:
+        j_o_2 = lower_right_pix_row
+
+
+    # region of the final image that is inside the original image, and whose content will be taken from the orig im
+    # (i = col, j=row)
+    #  _________________________________
+    # |                                | original image
+    # |   _____________________________|____
+    # |   |(j_f_1, i_f_1)              |    |
+    # |   |             (j_f_2, i_f_2) |    |
+    # |___|____________________________|    |
+    #     |                                 |
+    #     |_________________________________|  sliced final image
+
+    if upper_left_pix_col < 0:
+        i_f_1 = -upper_left_pix_col
+    else:
+        i_f_1 = 0
+    if upper_left_pix_row < 0:
+        j_f_1 = -upper_left_pix_row
+    else:
+        j_f_1 = 0
+
+    if lower_right_pix_col > orig_array.shape[1] - 1:
+        i_f_2 = (orig_array.shape[1] - 1) - upper_left_pix_col
+    else:
+        i_f_2 = num_cols - 1
+    if lower_right_pix_row > orig_array.shape[0] - 1:
+        j_f_2 = (orig_array.shape[0] - 1) - upper_left_pix_row
+    else:
+        j_f_2 = num_rows - 1
+
+    subarr[j_f_1:j_f_2 + 1, i_f_1:i_f_2 + 1] = orig_array[j_o_1:j_o_2 + 1, i_o_1:i_o_2 + 1]
+
+    return subarr
